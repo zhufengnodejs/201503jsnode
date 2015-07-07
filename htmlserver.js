060c2a7976a72c8454185c37ec9d21a29db349ed
zhuufengpeixun <zhufengpeixun@126.com>
2015-07-07 17:02:50 +0800

    image

--- htmlserver.js
+++ htmlserver.js
@@ -1,15 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 var mime = require('mime');
-
+var users = [];
 http.createServer(function(req,res){
     var url = req.url;
     if(url == '/'){
         fs.readFile("index.html","utf8",function(err,data){
             res.end(data);
         });
-    } else if(url == '/user'){
-        res.end('get a response');
+    } else if(url == '/addUser'){
+        req.on('data',function(chunk){
+            console.log(chunk);
+            users.push(chunk);
+        });
+        req.on('end',function(){
+            res.end(users);
+        })
     }else{
         res.setHeader('Content-Type',mime.lookup(url));
        fs.readFile(url.slice(1),function(err,data){
--- listener.js
+++ listener.js
@@ -5,7 +5,7 @@ window.onload = function () {
             xhr.open('post','/addUser');
             xhr.onreadystatechange = function(){
                 if(this.readyState == 4 && this.status == 200){
-
+                    document.getElementById('usersDiv').innerHTML = this.responseText;
                 }
             }
             xhr.send('username='+document.getElementById('username').value+'&password='+document.getElementById('password').value);
