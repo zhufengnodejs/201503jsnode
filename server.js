57f03643790f42947f68f8fce9034f7110d15d50
zhuufengpeixun <zhufengpeixun@126.com>
2015-07-07 12:29:28 +0800

    'menu'

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ var waiter = function(request,response){
     var url = request.url;
     var method = request.method;
     var host = request.headers['host'];
+    console.log(url);
     response.setHeader('Content-Type',
-        'text/html;charset=utf-8');
-    var menu = '<ul>';
-    menu+='<li>牛肉大葱包子</li>';
-    menu+='<li>鲜肉包子</li>';
-    menu+='<li>韭菜鸡蛋包子</li>';
-    menu+='<li>雪花啤酒</li>';
-    menu+='</ul>';
-    response.end(menu);//结束说话
+        'text/html;charset=UTF-8');
+    if(url=='/'){
+        var menu = '<ul>';
+        menu+='<li><a href="/niurou">牛肉大葱包子</a></li>';
+        menu+='<li><a href="/pork">鲜肉包子</a></li>';
+        menu+='<li><a href="/jiucaiegg">韭菜鸡蛋包子</a></li>';
+        menu+='<li><a href="/snowflower">雪花啤酒</a></li>';
+        menu+='</ul>';
+        response.end(menu);//结束说话
+    }else if(url=='/niurou'){
+        response.end('给你一个牛肉大葱包子');//结束说话
+    }else if(url=='/pork'){
+        response.end('给你一个鲜肉包子');//结束说话
+    }else if(url=='/jiucaiegg'){
+        response.end('给你一个韭菜鸡蛋包子');//结束说话
+    }else if(url=='/snowflower'){
+        response.end('给你一个雪花啤酒');//结束说话
+    }
 }
 
 //创建霍营包子店
