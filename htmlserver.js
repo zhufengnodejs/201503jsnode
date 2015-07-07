42f14a3af4b65a6cbfb8c201cd6f080adb4f67bc
zhuufengpeixun <zhufengpeixun@126.com>
2015-07-07 15:55:05 +0800

    image

--- htmlserver.js
+++ htmlserver.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+var mime = require('mime');
+
 http.createServer(function(req,res){
     var url = req.url;
     if(url == '/'){
         fs.readFile("index.html","utf8",function(err,data){
             res.end(data);
         });
-
-    }else if(url == '/listener.js'){
-       fs.readFile("listener1.js","utf8",function(err,data){
+    }else{
+        res.setHeader('Content-Type',mime.lookup(url));
+       fs.readFile(url.slice(1),function(err,data){
            if(err){
                res.end('not found');
            }else{
