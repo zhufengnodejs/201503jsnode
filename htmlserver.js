cda971f81e13683b37d0064666d66d3ffe5b5c0d
zhuufengpeixun <zhufengpeixun@126.com>
2015-07-07 15:13:56 +0800

    add listener

--- htmlserver.js
+++ htmlserver.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function(req,res){
     var url = req.url;
     if(url == '/'){
-        
+        var content=fs.readFileSync("index.html","utf8");
+        res.end(content);
+    }else if(url == '/listener.js'){
+        var content=fs.readFileSync("listener.js","utf8");
+        res.end(content);
     }
+
 }).listen(8080);
---
+++ listener.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById('btnGet')
+        .addEventListener('click', function () {
+            document.getElementById('getDiv')
+                .innerHTML = 'OK';
+        });
+}
